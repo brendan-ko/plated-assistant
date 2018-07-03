48888edf04cf21aa0eb5307810b0d6418f1f301e
Brendan Ko <brendan.ko@plated.com>
2018-07-03 16:17:02 -0400

    Add commented handler function for welcome axios request

--- getAxios.js
+++ getAxios.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 function getBoxes(configHeader) {
   return axios.get('https://pltd-staging.com/api/v2/boxes/future.json', configHeader)
   .then(boxRes => {
-    console.log(boxRes.data);
     return boxRes.data.boxes;
   })
   .catch(error => {
@@ -16,7 +15,6 @@ function getBoxes(configHeader) {
 function getUser(configHeader) {
   return axios.get('https://pltd-staging.com/api/v2/me.json', configHeader)
   .then(userRes => {
-    console.log(userRes.data);
     return userRes.data.user;
   })
   .catch(error => {
@@ -26,9 +24,34 @@ function getUser(configHeader) {
   })
 }
 
+// function handleWelcomeAxiosRequest(configHeader) {
+//   const user = getUser(configHeader);
+//   const boxes = getBoxes(configHeader);
+
+//   return Promise.all([boxes, user])
+//     .then(([boxesRes, userRes]) => {
+//       const userContext = {
+//         'name': 'user',
+//         'lifespan': 5,
+//         'parameters': { 'user': userRes }
+//       };
+//       const boxesContext = {
+//         'name': 'boxes',
+//         'lifespan': 5,
+//         'parameters': { 'boxes': boxesRes }
+//       };
+//       return {
+//         'userContext': userContext,
+//         'boxesContext': boxesContext,
+//       }
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     })
+// }
 
 
 module.exports = {
-  getBoxes: getBoxes,
-  getUser: getUser,
+  'getBoxes': getBoxes,
+  'getUser': getUser,
 };
